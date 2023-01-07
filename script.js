99a0b877691e00fff5aee0976ec31400a1b32859
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-01-07 22:05:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const retrieveInfoContainer = document.querySelector(".retrieve-info-container")
 const addBtn = document.querySelector("#add-btn");
 const tableBody = document.querySelector("#table-body");
 const inputsRow = document.querySelector("#inputs-row");
+const aadharInputBox= document.querySelector("#aadhar-input-box");
+const findBtn = document.querySelector("#find-btn");
+const showInfoTable = document.querySelector("#show-info-table");
 
 
 let isView=true;
@@ -20,7 +23,8 @@ function hideShow(){
     }
     
 }
-
+let count =1;
+const listArr=[];
 function addRow(){
    
         const tr = document.createElement("tr")
@@ -41,8 +45,10 @@ function addRow(){
 
         const saveBtn = document.createElement("a");
         saveBtn.innerText="Save";
+        saveBtn.id=`saveBtn${count}`;
         const deleteBtn = document.createElement("a");
         deleteBtn.innerText="Delete";
+        deleteBtn.id=`deleteBtn${count}`;
 
         inputName.type="text";
         inputDob.type="text";
@@ -58,17 +64,91 @@ function addRow(){
         tdActions.appendChild(saveBtn);
         tdActions.appendChild(deleteBtn);
 
-        tr.appendChild(tdName);
-        tr.appendChild(tdDob);
-        tr.appendChild(tdAadhar);
-        tr.appendChild(tdMobile);
-        tr.appendChild(tdAge);
-        tr.appendChild(tdActions);
+        tr.append(tdName,tdDob,tdAadhar,tdMobile,tdAge,tdActions);
         tableBody.appendChild(tr);
+
+        saveBtn.addEventListener("click",() => {
+            console.log(inputName.value)
+            const obj = {
+                "Name":inputName.value,
+                "Date_of_Birth":inputDob.value,
+                "Aadhar_Number":inputAadhar.value,
+                "Mobile_Number":inputMobile.value,
+                "Age":23
+            };
+            listArr.push(obj);
+            localStorage.setItem("list",JSON.stringify(listArr));
+            // console.log(localStorage.getItem("list"))
+        });
+        deleteBtn.addEventListener("click",() => {
+            tableBody.removeChild(tr);
+        });
+
+        count++;
  
     
 }
 
+function getData() {
+    const aadharNumber = aadharInputBox.value;
+
+    let getList = JSON.parse(localStorage.getItem("list"));
+    let result = getList.filter((element,index) => {
+        if(element.Aadhar_Number == aadharNumber)
+        {
+            return true;
+        }
+        
+    })
+
+   
+    result.forEach((element ,idx) => {
+        showInfoTable.innerHTML="";
+        
+        let trName = document.createElement("tr");
+        let trDob= document.createElement("tr");
+        let trAadhar= document.createElement("tr");
+        let trMobile= document.createElement("tr");
+        let trAge= document.createElement("tr");
+
+        let tdNameTitle = document.createElement("td");
+        tdNameTitle.innerText="Name";
+        let tdNameValue = document.createElement("td");
+        tdNameValue.innerText=element.Name;
+
+        let tdDobTitle = document.createElement("td");
+        tdDobTitle .innerText="DOB";
+        let tdDobValue = document.createElement("td");
+        tdDobValue.innerText=element.Date_of_Birth;
+
+        let tdAadharTitle = document.createElement("td");
+        tdAadharTitle .innerText="Aadhar No.";
+        let tdAadharValue = document.createElement("td");
+        tdAadharValue.innerText=element.Aadhar_Number;
+
+        let tdMobileTitle = document.createElement("td");
+        tdMobileTitle .innerText="Mobile";
+        let tdMobileValue = document.createElement("td");
+        tdMobileValue.innerText=element.Mobile_Number;
+
+        let tdAgeTitle = document.createElement("td");
+        tdAgeTitle.innerText="Age";
+        let tdAgeValue = document.createElement("td");
+        tdAgeValue.innerText=element.Age;
+
+        trName.append(tdNameTitle,tdNameValue);
+        trDob.append(tdDobTitle ,tdDobValue);
+        trAadhar.append(tdAadharTitle ,tdAadharValue);
+        trMobile.append(tdMobileTitle  ,tdMobileValue);
+        trAge.append(tdAgeTitle  ,tdAgeValue);
+
+        showInfoTable.append(trName,trDob,trAadhar,trMobile,trAge);
+    })
+   
+    console.log(result);
+}
+
+findBtn.addEventListener("click",getData);
 addPersonBtn.addEventListener("click",hideShow);
 retrieveInfoBtn.addEventListener("click",hideShow);
 addBtn.addEventListener("click",addRow);
