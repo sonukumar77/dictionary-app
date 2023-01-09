ec26ef679475cddb35d055633b469c35e0cdd6c7
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-01-09 23:43:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,154 +1,185 @@
 const addPersonBtn = document.querySelector("#add-new-person-btn");
 const retrieveInfoBtn = document.querySelector("#retrieve-info-btn");
 const addPersonContainer = document.querySelector(".add-new-person-container");
-const retrieveInfoContainer = document.querySelector(".retrieve-info-container");
+const retrieveInfoContainer = document.querySelector(
+  ".retrieve-info-container"
+);
 const addBtn = document.querySelector("#add-btn");
 const tableBody = document.querySelector("#table-body");
 const inputsRow = document.querySelector("#inputs-row");
-const aadharInputBox= document.querySelector("#aadhar-input-box");
+const aadharInputBox = document.querySelector("#aadhar-input-box");
 const findBtn = document.querySelector("#find-btn");
 const showInfoTable = document.querySelector("#show-info-table");
 
-
-let isView=true;
-function hideShow(){
-    if(isView == true){
-        addPersonContainer.style.display="none";
-        retrieveInfoContainer.style.display="block";
-        isView=false;
-    }else{
-        addPersonContainer.style.display="block";
-        retrieveInfoContainer.style.display="none";
-        isView=true;
-    }
-    
+const dt = new Date();
+let isView = true;
+function hideShow() {
+  if (isView == true) {
+    addPersonContainer.style.display = "none";
+    retrieveInfoContainer.style.display = "block";
+    isView = false;
+  } else {
+    addPersonContainer.style.display = "block";
+    retrieveInfoContainer.style.display = "none";
+    isView = true;
+  }
 }
-let count =1;
-const listArr=[];
-function addRow(){
-   
-        const tr = document.createElement("tr")
+let count = 1;
+const listArr = [];
+function addRow() {
+  const tr = document.createElement("tr");
 
-        const p = document.createElement("p")
+  const p = document.createElement("p");
 
-        const tdName = document.createElement("td")
-        const tdDob = document.createElement("td")
-        const tdAadhar = document.createElement("td")
-        const tdMobile = document.createElement("td")
-        const tdAge = document.createElement("td")
-        const tdActions = document.createElement("td")
+  const tdName = document.createElement("td");
+  const tdDob = document.createElement("td");
+  const tdAadhar = document.createElement("td");
+  const tdMobile = document.createElement("td");
+  const tdAge = document.createElement("td");
+  const tdActions = document.createElement("td");
 
-        const inputName = document.createElement("input");
-        const inputDob = document.createElement("input");
-        const inputAadhar = document.createElement("input");
-        const inputMobile = document.createElement("input");
+  const inputName = document.createElement("input");
+  const inputDob = document.createElement("input");
+  const inputAadhar = document.createElement("input");
+  const inputMobile = document.createElement("input");
 
-        const saveBtn = document.createElement("a");
-        saveBtn.innerText="Save";
-        saveBtn.id=`saveBtn${count}`;
-        const deleteBtn = document.createElement("a");
-        deleteBtn.innerText="Delete";
-        deleteBtn.id=`deleteBtn${count}`;
+  const saveBtn = document.createElement("a");
+  saveBtn.innerText = "Save";
+  saveBtn.id = `saveBtn${count}`;
+  saveBtn.style.textDecoration = "underline";
+  const deleteBtn = document.createElement("a");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.style.textDecoration = "underline";
+  deleteBtn.style.marginLeft = "5px";
+  deleteBtn.id = `deleteBtn${count}`;
 
-        inputName.type="text";
-        inputDob.type="text";
-        inputAadhar.type="text";
-        inputMobile.type="text";
+  inputName.type = "text";
+  inputDob.type = "text";
+  inputDob.placeholder = "dd/mm/yyyy";
+  inputAadhar.type = "text";
+  inputMobile.type = "text";
 
-        tdName.appendChild(inputName);
-        tdDob.appendChild(inputDob);
-        tdAadhar.appendChild(inputAadhar);
-        tdMobile.appendChild(inputMobile);
-        tdAge.appendChild(p);
+  tdName.appendChild(inputName);
+  tdDob.appendChild(inputDob);
+  tdAadhar.appendChild(inputAadhar);
+  tdMobile.appendChild(inputMobile);
+  tdAge.appendChild(p);
 
-        tdActions.appendChild(saveBtn);
-        tdActions.appendChild(deleteBtn);
+  tdActions.appendChild(saveBtn);
+  tdActions.appendChild(deleteBtn);
 
-        tr.append(tdName,tdDob,tdAadhar,tdMobile,tdAge,tdActions);
-        tableBody.appendChild(tr);
+  tr.append(tdName, tdDob, tdAadhar, tdMobile, tdAge, tdActions);
+  tableBody.appendChild(tr);
 
-        saveBtn.addEventListener("click",() => {
-            console.log(inputName.value)
-            const obj = {
-                "Name":inputName.value,
-                "Date_of_Birth":inputDob.value,
-                "Aadhar_Number":inputAadhar.value,
-                "Mobile_Number":inputMobile.value,
-                "Age":23
-            };
-            listArr.push(obj);
-            localStorage.setItem("list",JSON.stringify(listArr));
-            // console.log(localStorage.getItem("list"))
-        });
-        deleteBtn.addEventListener("click",() => {
-            tableBody.removeChild(tr);
-        });
+  saveBtn.addEventListener("click", () => {
+    if (
+      inputName.value == "" &&
+      inputDob.value == "" &&
+      inputAadhar.value == "" &&
+      inputMobile.value == ""
+    ) {
+      alert("All fields is required!");
+      return false;
+    } else if (inputAadhar.value.length != 12) {
+      alert("Aadhar number must be of 12 digit!");
+      return false;
+    } else if (inputMobile.value.length != 10) {
+      alert("Mobile number must be of 10 digit!");
+      return false;
+    } else {
+      p.innerText = dt.getFullYear() - inputDob.value.split("/")[2];
 
-        count++;
- 
-    
+      const obj = {
+        Name: inputName.value,
+        Date_of_Birth: inputDob.value,
+        Aadhar_Number: inputAadhar.value,
+        Mobile_Number: inputMobile.value,
+        Age: p.innerText,
+      };
+
+      listArr.push(obj);
+      localStorage.setItem("list", JSON.stringify(listArr));
+    }
+  });
+  deleteBtn.addEventListener("click", () => {
+    const localData = JSON.parse(localStorage.getItem("list"));
+
+    for (let i = 0; i < localData.length; i++) {
+      if (inputAadhar.value == localData[i].Aadhar_Number) {
+        localData.splice(i, 1);
+        localStorage.setItem("list", JSON.stringify(localData));
+        tableBody.removeChild(tr);
+      }
+    }
+  });
+
+  count++;
 }
 
 function getData() {
-    const aadharNumber = aadharInputBox.value;
+  const aadharNumber = aadharInputBox.value;
+  if (aadharNumber.length != 12) {
+    alert("Aadhar number must be of 12 digit!");
+    aadharInputBox.value = "";
+    return false;
+  }
 
-    let getList = JSON.parse(localStorage.getItem("list"));
-    let result = getList.filter((element,index) => {
-        if(element.Aadhar_Number == aadharNumber)
-        {
-            return true;
-        }
-        
-    })
+  let getList = JSON.parse(localStorage.getItem("list"));
+  let result = getList.filter((element, index) => {
+    if (element.Aadhar_Number == aadharNumber) {
+      return true;
+    }
+  });
 
-   
-    result.forEach((element ,idx) => {
-        showInfoTable.innerHTML="";
-        
-        let trName = document.createElement("tr");
-        let trDob= document.createElement("tr");
-        let trAadhar= document.createElement("tr");
-        let trMobile= document.createElement("tr");
-        let trAge= document.createElement("tr");
+  if (result.length != 0) {
+    result.forEach((element, idx) => {
+      showInfoTable.innerHTML = "";
 
-        let tdNameTitle = document.createElement("td");
-        tdNameTitle.innerText="Name";
-        let tdNameValue = document.createElement("td");
-        tdNameValue.innerText=element.Name;
+      let trName = document.createElement("tr");
+      let trDob = document.createElement("tr");
+      let trAadhar = document.createElement("tr");
+      let trMobile = document.createElement("tr");
+      let trAge = document.createElement("tr");
 
-        let tdDobTitle = document.createElement("td");
-        tdDobTitle .innerText="DOB";
-        let tdDobValue = document.createElement("td");
-        tdDobValue.innerText=element.Date_of_Birth;
+      let tdNameTitle = document.createElement("td");
+      tdNameTitle.innerText = "Name";
+      let tdNameValue = document.createElement("td");
+      tdNameValue.innerText = element.Name;
 
-        let tdAadharTitle = document.createElement("td");
-        tdAadharTitle .innerText="Aadhar No.";
-        let tdAadharValue = document.createElement("td");
-        tdAadharValue.innerText=element.Aadhar_Number;
+      let tdDobTitle = document.createElement("td");
+      tdDobTitle.innerText = "DOB";
+      let tdDobValue = document.createElement("td");
+      tdDobValue.innerText = element.Date_of_Birth;
 
-        let tdMobileTitle = document.createElement("td");
-        tdMobileTitle .innerText="Mobile";
-        let tdMobileValue = document.createElement("td");
-        tdMobileValue.innerText=element.Mobile_Number;
+      let tdAadharTitle = document.createElement("td");
+      tdAadharTitle.innerText = "Aadhar No.";
+      let tdAadharValue = document.createElement("td");
+      tdAadharValue.innerText = element.Aadhar_Number;
 
-        let tdAgeTitle = document.createElement("td");
-        tdAgeTitle.innerText="Age";
-        let tdAgeValue = document.createElement("td");
-        tdAgeValue.innerText=element.Age;
+      let tdMobileTitle = document.createElement("td");
+      tdMobileTitle.innerText = "Mobile";
+      let tdMobileValue = document.createElement("td");
+      tdMobileValue.innerText = element.Mobile_Number;
 
-        trName.append(tdNameTitle,tdNameValue);
-        trDob.append(tdDobTitle ,tdDobValue);
-        trAadhar.append(tdAadharTitle ,tdAadharValue);
-        trMobile.append(tdMobileTitle  ,tdMobileValue);
-        trAge.append(tdAgeTitle  ,tdAgeValue);
+      let tdAgeTitle = document.createElement("td");
+      tdAgeTitle.innerText = "Age";
+      let tdAgeValue = document.createElement("td");
+      tdAgeValue.innerText = element.Age;
 
-        showInfoTable.append(trName,trDob,trAadhar,trMobile,trAge);
-    })
-   
-    console.log(result);
+      trName.append(tdNameTitle, tdNameValue);
+      trDob.append(tdDobTitle, tdDobValue);
+      trAadhar.append(tdAadharTitle, tdAadharValue);
+      trMobile.append(tdMobileTitle, tdMobileValue);
+      trAge.append(tdAgeTitle, tdAgeValue);
+
+      showInfoTable.append(trName, trDob, trAadhar, trMobile, trAge);
+    });
+  } else {
+    alert("No match found");
+  }
 }
 
-findBtn.addEventListener("click",getData);
-addPersonBtn.addEventListener("click",hideShow);
-retrieveInfoBtn.addEventListener("click",hideShow);
-addBtn.addEventListener("click",addRow);
+findBtn.addEventListener("click", getData);
+addPersonBtn.addEventListener("click", hideShow);
+retrieveInfoBtn.addEventListener("click", hideShow);
+addBtn.addEventListener("click", addRow);
